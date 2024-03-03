6aa6cc4d74ee303dcd68a1a500e55f89aaa5b6d6
Celso Herib <102439750+CelsoHerib@users.noreply.github.com>
2024-03-02 22:01:10 -0300

    update js

--- Music Player/script.js
+++ Music Player/script.js
@@ -108,6 +108,28 @@ const pauseSong = () => {
   audio.pause();
 };
 
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
+  }
+};
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) {
+    return;
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+    playSong(previousSong.id);
+  }
+};
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+ }
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -131,6 +153,12 @@ const renderSongs = (array) => {
 };
 
 pauseButton.addEventListener("click", pauseSong);
+nextButton.addEventListener("click", playNextSong);
+previousButton.addEventListener("click",  playPreviousSong);
+
+const getCurrentSongIndex = () => {
+  return userData?.songs.indexOf(userData?.currentSong);
+};
 
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
