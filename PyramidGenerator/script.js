8b5bb720b57555ce3a8bdfb4c3d16f3dd2ea6ddd
CelsoHerib <102439750+CelsoHerib@users.noreply.github.com>
2024-06-27 20:32:52 -0300

    nueva funcion Js

--- PyramidGenerator/script.js
+++ PyramidGenerator/script.js
@@ -2,11 +2,9 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-    return character + name;
-  }
-  const call = padRow("CamperChan");
-  console.log(call);
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
   
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
