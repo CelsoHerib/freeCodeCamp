8d1231ae5b516b899eb46a1bd7ef49e510e7e9a0
CelsoHerib <102439750+CelsoHerib@users.noreply.github.com>
2024-08-25 21:46:15 -0300

    primera parte

---
+++ dateFormatter/script.js
@@ -0,0 +1,6 @@
+const currentParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+
+const date = new Date();
+console.log(date);
