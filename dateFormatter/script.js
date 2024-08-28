3eba653d6e713a74f76a0b41474b78fdb90e7c10
CelsoHerib <102439750+CelsoHerib@users.noreply.github.com>
2024-08-28 19:02:58 -0300

    js modificaciones

--- dateFormatter/script.js
+++ dateFormatter/script.js
@@ -3,4 +3,17 @@ const dateOptionsSelectElement = document.getElementById("date-options");
 
 
 const date = new Date();
-console.log(date);
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const ampm = hours >= 12 ? 'PM' : 'AM';
+const formattedHours = hours % 12;
+const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+const formattedTime = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds + ' ' +
+ampm;
+const formattedDate = `${day}-${month}-${year}`;
+const formattedDateTime = formattedDate + ' ' + formattedTime;
